6dc937fae25302e4fa0c00528299a3525a568ba8
dali546 <35352237+dali546@users.noreply.github.com>
2019-03-28 11:14:24 +0000

    Refactored Code
    Code is now easier to read and straight forward. Test still passed :+1:  :smiley:

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -8,55 +8,35 @@ class WeirdShop {
     }
 
     void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage Pass")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Gold Coin")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage Pass")) {
-                        if (items[i].sellIn < 12) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 7) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Gold Coin")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage Pass")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Gold Coin")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        String name;
+        for (Item item : items) {
+            name = item.name;
+            if (name.equals("Backstage Pass")) {
+                updateBackStage(item);
+            } else if (name.equals("Aged Brie")) {
+                updateAgedBrie(item);
+            } else if (!name.equals("Gold Coin")) {
+                updateOtherItems(item);
             }
         }
+    }
+
+    private void updateAgedBrie(Item item) {
+        item.sellIn--;
+        if (item.quality<50) item.quality++;
+    }
+
+    private void updateBackStage(Item item) {
+        item.sellIn--;
+        if (item.quality < 50) item.quality++;
+        if (item.quality < 50 && item.sellIn <= 10) item.quality++;
+        if (item.quality < 50 && item.sellIn <= 5) item.quality++;
+        if (item.sellIn < 0) item.quality = 0;
+    }
+
+    private void updateOtherItems(Item item) {
+        item.sellIn--;
+        if (item.quality > 0) item.quality--;
+        if (item.sellIn < 0 && item.quality>0) item.quality--;
     }
 }
