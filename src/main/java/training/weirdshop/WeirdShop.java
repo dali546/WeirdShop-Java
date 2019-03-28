f899ae6d278985c1ec7bf3fb5b90db59b0719a32
dali546 <35352237+dali546@users.noreply.github.com>
2019-03-28 11:42:58 +0000

    Premium Items Refactor
    - Made Test pass for Premium Items

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -8,35 +8,48 @@ class WeirdShop {
     }
 
     void updateQuality() {
-        String name;
         for (Item item : items) {
-            name = item.name;
-            if (name.equals("Backstage Pass")) {
-                updateBackStage(item);
-            } else if (name.equals("Aged Brie")) {
-                updateAgedBrie(item);
-            } else if (!name.equals("Gold Coin")) {
-                updateOtherItems(item);
-            }
+            boolean isPremium = item.name.contains("Premium");
+            if (item.name.contains("Backstage Pass")) updateBackStage(item, isPremium);
+            else if (item.name.contains("Aged Brie")) updateAgedBrie(item, isPremium);
+            else if (!item.name.contains("Gold Coin")) updateOtherItems(item, isPremium);
         }
     }
 
-    private void updateAgedBrie(Item item) {
+    private void updateAgedBrie(Item item, boolean isPremium) {
         item.sellIn--;
-        if (item.quality<50) item.quality++;
+        if (item.quality < 50) increaseQuality(item, isPremium);
+        capQuality(item);
     }
 
-    private void updateBackStage(Item item) {
+    private void updateBackStage(Item item, boolean isPremium) {
         item.sellIn--;
-        if (item.quality < 50) item.quality++;
-        if (item.quality < 50 && item.sellIn <= 10) item.quality++;
-        if (item.quality < 50 && item.sellIn <= 5) item.quality++;
+        if (item.quality < 50) increaseQuality(item, isPremium);
+        if (item.quality < 50 && item.sellIn <= 10) increaseQuality(item, isPremium);
+        if (item.quality < 50 && item.sellIn <= 5) increaseQuality(item, isPremium);
         if (item.sellIn < 0) item.quality = 0;
+        capQuality(item);
     }
 
-    private void updateOtherItems(Item item) {
+    private void updateOtherItems(Item item, boolean isPremium) {
         item.sellIn--;
-        if (item.quality > 0) item.quality--;
-        if (item.sellIn < 0 && item.quality>0) item.quality--;
+        if (item.quality > 0) decreaseQuality(item, isPremium);
+        if (item.sellIn < 0 && item.quality > 0) decreaseQuality(item, isPremium);
+        capQuality(item);
+    }
+
+    private void capQuality(Item item) {
+        if (item.quality < 0) item.quality = 0;
+        if (item.quality > 50) item.quality = 50;
+    }
+
+    private void decreaseQuality(Item item, boolean isPremium) {
+        if (isPremium) item.quality--;
+        item.quality--;
+    }
+
+    private void increaseQuality(Item item, boolean isPremium) {
+        if (isPremium) item.quality++;
+        item.quality++;
     }
 }
